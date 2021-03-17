3a4a08271be68af003f3440527596ff3c61902de
Di Qian <diqian@gmail.com>
2021-03-16 20:26:13 -0400

    authentication and authorization added

--- src/main/java/com/jrp/pma/controllers/EmployeeController.java
+++ src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -41,6 +41,6 @@ public class EmployeeController {
 		empRepo.save(employee);
 		
 		//use a redirect to prevent duplicate submissions
-		return "redirect:/employees/new";
+		return "redirect:/employees";
 	}
 }
